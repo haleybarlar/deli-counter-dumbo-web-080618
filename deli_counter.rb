9f81034ef0c65548b5122b90c64cee474aeded4e
Haley Someone <hbarlar1@gmail.com>
2018-07-24 00:03:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,5 @@
 def line(katz_line)
-  if katz_line.length == 0
-    puts "The line is currently empty."
-  else
-    message = "The line is currently:"
+  if katz_line.length > 0
     
-    katz_line.each do |value, i|
-      message += " "
 end
   end
