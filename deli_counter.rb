6f047de3e2aae861f0898053557140741ddbd831
Haley Someone <hbarlar1@gmail.com>
2018-07-24 00:11:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,2 @@
 def line(katz_line)
-  if katz_line.length > 0
-    newArray = []
-    i = 1
-    katz_line.each do |name|
-      newArray.push("#{i}. #{name}")
-      i += 1
-    puts "The line is currently: #{newArray.join(" ")}"
-  end
-    puts "The line is currently empty."
-end
-end
+  if katz_line.length == 0
