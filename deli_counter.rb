7f2fcecbea3e0c197eb5cd71209fe4a0b4531f83
Haley Someone <hbarlar1@gmail.com>
2018-07-24 00:12:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,9 @@ def line(katz_line)
   else
     message = "The line is currently:"
     
+      array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
