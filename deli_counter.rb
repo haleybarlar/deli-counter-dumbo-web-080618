84e3fdace114d4bed6e09ae5f9a0554a7667dcb9
Haley Someone <hbarlar1@gmail.com>
2018-07-24 00:10:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_line)
     i = 1
     katz_line.each do |name|
       newArray.push("#{i}. #{name}")
-      i++
+      i += 1
     puts "The line is currently: #{newArray.join(" ")}"
   else
     puts "The line is currently empty."
