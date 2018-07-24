f05c46dbebeed726b70bc34211cbf23ad57f986f
Haley Someone <hbarlar1@gmail.com>
2018-07-24 00:21:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,11 @@ def take_a_number(katz_line, name)
   katz_line.push(name)
   puts "Welcome, #{name}. You are number #{katz_line.length} in line."
 end
+
+def now_serving(katz_line)
+  if katz_line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_line.shift()."
+  end
+end 
